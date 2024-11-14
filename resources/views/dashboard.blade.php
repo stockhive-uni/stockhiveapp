e80aebb7b1987eb0de3a027ed338d5d5c4347ca7
ajstrongdev <ajstrongdev@proton.me>
2024-11-14 22:28:57 +0000

    Fix: name not displaying on dashboard
    This is the result of the database using user()->name to both user()->first_name & user()->last_name

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
          <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-stockhive-grey-dark text-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <p class="text-xl"><span class="font-bold">Name:</span> {{Auth::user()->name }}</p>
+                    <p class="text-xl"><span class="font-bold">Name:</span> {{Auth::user()->first_name }} {{Auth::user()->last_name}}</p>
                     <p class="text-xl"><span class="font-bold">Email:</span> {{ Auth::user()->email }}</p>
                     <p class="text-xl"><span class="font-bold">Employee ID:</span> {{ Auth::user()->id}}</p>
                 </div> 
