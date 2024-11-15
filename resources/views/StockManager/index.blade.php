a5ca9f066a1f9875a1219d3633b0acc7a3b808b0
ajstrongdev <ajstrongdev@proton.me>
2024-11-15 13:50:42 +0000

    Style: stock manager page

--- resources/views/StockManager/index.blade.php
+++ resources/views/StockManager/index.blade.php
@@ -1,21 +1,36 @@
 <x-app-layout>
     <form action='{{ route('stock.chosenItems')}}' method="POST">
     @csrf
-    <ul>
-        <dt>ID</dt>
-        <dt>name</dt>
-        <dt>price</dt>
-        <dt>department id</dt>
-    @forelse ($items as $item)
-            <li>{{$item->id}}</li>
-            <li>{{$item->name}}</li>
-            <li>£{{$item->price}}</li>
-            <li>{{$item->department->name}}</li>
-            <input type="checkbox" name='items[]' value={{$item->id}}>
-        </ul>
-    @empty
-    <div>No Stock Available To Order</div>
-    @endforelse
-    <x-primary-button>Create Order</x-primary-button>
-    </form>
+
+    <div class="py-12">
+         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-stockhive-grey-dark text-white overflow-hidden shadow-sm sm:rounded-lg">
+                @if($items -> isNotEmpty())
+                <table class="border-separate border-2 m-auto mb-12 w-[90%] text-center border-spacing-8 bg-stockhive-grey rounded-lg">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Department</th>
+                            <th>Select</th>
+                        </tr>
+                    </thead>
+                    @foreach ($items as $item)
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>£{{ $item->price }}</td>
+                        <td>{{ $item->department->name }}</td>
+                        <td><input type="checkbox" name="items[]" value="{{ $item->id }}"></td>
+                    </tr>
+                    @endforeach
+                @else
+                    <p class="text-white">No available stock to order</p>
+                @endif
+                <div class="m-auto w-[90%] text-right"><x-primary-button>Create Order</x-primary-button></div>
+                </form>
+        </div>
+    </div>
+</div>
 </x-app-layout>
