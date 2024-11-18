365831dd19126c3512d1845f6ed26b03b78cf8e1
ajstrongdev <ajstrongdev@proton.me>
2024-11-18 21:51:57 +0000

    Fix: paginate component structure issues

--- resources/views/components/paginate.blade.php
+++ resources/views/components/paginate.blade.php
@@ -1,5 +1,4 @@
 @props(['items'])
-<div class="flex justify-between items-center gap-8 my-4 border-grey bg-stockhive-grey rounded-lg p-4 border-2 m-auto w-[90%] text-right">
+<div class="bg-stockhive-grey-dark rounded-lg px-4">
     {{ $items->appends(request()->except('page'))->links()}}
-    <x-primary-button>Create Order</x-primary-button>
 </div>
