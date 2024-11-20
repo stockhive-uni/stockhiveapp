85ab8f68732b0b4e816f1974d125cb170725569e
Web Extension <robertjenner5@outlook.com>
2024-11-20 12:00:17 +0000

    Fixed styling differences

--- resources/views/StockManager/order.blade.php
+++ resources/views/StockManager/order.blade.php
@@ -1,16 +1,14 @@
 <x-app-layout>
     <div class="bg-stockhive-grey-dark text-white shadow-sm rounded-lg mt-8 lg:w-[85%] w-full m-auto p-4">
         <!-- Sorting Form -->
-        <form method="GET" action="{{ route('stock-management.sortOrder') }}" class="m-auto text-right w-[100%] mb-2">
-            <div class="flex items-center justify-end gap-1 w-full">
-                <!-- Select dropdown and Sort button aligned -->
-                <select name="sort" id="sort" class="text-white bg-stockhive-grey hover:shadow-bxs hover:border-accent transition-all hover:ring-accent p-2 rounded-lg w-[30%]">
-                    <option value="id" {{ request('sort') === 'id' ? 'selected' : '' }}>ID</option>
-                    <option value="name" {{ request('sort') === 'name' ? 'selected' : '' }}>Name</option>
-                    <option value="price" {{ request('sort') === 'price' ? 'selected' : '' }}>Price</option>
-                </select>
-                <x-primary-button class="ml-4">Sort</x-primary-button>
-            </div>
+        <form method="GET" action="{{ route('stock-management.sortOrder') }}" class="m-auto text-right w-[90%]">
+            <!-- Select dropdown and Sort button aligned -->
+            <select name="sort" id="sort" class="text-white bg-stockhive-grey hover:shadow-bxs hover:border-accent transition-all hover:ring-accent p-2 rounded-lg w-[50%]">
+                <option value="id" {{ request('sort') === 'id' ? 'selected' : '' }}>ID</option>
+                <option value="name" {{ request('sort') === 'name' ? 'selected' : '' }}>Name</option>
+                <option value="price" {{ request('sort') === 'price' ? 'selected' : '' }}>Price</option>
+            </select>
+            <x-primary-button class="ml-4">Sort</x-primary-button>
             <input type="hidden" name="count" value="{{ $count }}">
             <input type="hidden" name="deliveryDate" value="{{ $deliveryDate }}">
             <input type="hidden" name="totalPrice" value="{{ $totalPrice }}">
@@ -24,13 +22,13 @@
             @csrf
             @if($items->isNotEmpty())
                 <!-- Pagination and Order Button -->
-                <div class="flex justify-between items-center gap-8 my-4 border-grey bg-stockhive-grey rounded-lg p-4 border-2 w-full">
+                <div class="flex justify-between items-center gap-8 my-4 border-grey bg-stockhive-grey rounded-lg p-4 border-2 m-auto w-[90%] text-right">
                     <x-paginate :items="$items"/>
-                    <x-primary-button>Create Order</x-primary-button>
+                    <x-primary-button>Place Order</x-primary-button>
                 </div>
 
                 <!-- Items Table -->
-                <table class="border-collapse border-2 w-full text-center border-grey hover:border-accent transition-all hover:shadow-bxs bg-stockhive-grey rounded-lg">
+                <table class="border-separate border-2 m-auto my-4 lg:w-[90%] w-full text-center border-grey hover:border-accent transition-all hover:shadow-bxs border-spacing-2 md:border-spacing-8 bg-stockhive-grey rounded-lg">
                     <thead>
                         <tr class="text-left border-b-2 border-grey">
                             <th class="py-2 px-4 text-center">ID</th>
