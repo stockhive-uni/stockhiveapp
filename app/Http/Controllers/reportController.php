c16901be0e75987d26ca421e53dc0822537732ef
ajstrongdev <ajstrongdev@proton.me>
2024-11-22 12:44:00 +0000

    Start: chartjs work
    This is very broken rn

--- app/Http/Controllers/reportController.php
+++ app/Http/Controllers/reportController.php
@@ -3,16 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
+use Illuminate\Support\Facades\DB;
 
 class reportController extends Controller
 {
-        
-    //redirect to report page
     public function index(Request $request) 
     {
         //here we use chart.js with the id of the item and orders to make a chart of order history
-
-        
-        return view('StockManager.report', ['request' => $request]);
+        $stock = Item::whereIn('id', $request->items)->with('department')->get();
+        $allresults = array();
+        foreach ($stock as $stockitem) {
+            foreach ($stockitem as $item) {
+                $id=$item->id;
+                $orders = $item->orders;
+                $query = DB::query('SELECT Transaction_Item.quantity, Transaction_Item.price, Transaction.date_time  FROM Transaction_Item, Transaction WHERE Transaction_Item.item_id = $id AND Transaction_Item.transaction_id = Transaction.id', [$id]);
+                $quantity = $query->pluck('quantity');
+                $price = $query->pluck('price');
+                $date = $query->pluck('date_time');
+                $month = date('m', strtotime($date)); // Get month from date
+                $total = $quantity * $price;
+                array_push($allresults, compact('total', 'month'));
+            }
+        }   
+        return view('StockManager.report', ['allresults' => $allresults]);
     } 
 }
--- resources/views/StockManager/report.blade.php
+++ resources/views/StockManager/report.blade.php
@@ -1,4 +1,7 @@
 <x-app-layout>
+
+    @dd ($allresults)
+
     @foreach ($request->input('reports') as $report)
         <div>{{$report}}</div>
     @endforeach
