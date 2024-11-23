e3a0e3d7a4584f8c348142a00dd3a4878a6dbe3f
ajstrongdev <ajstrongdev@proton.me>
2024-11-23 14:19:20 +0000

    Fix: homepage styling
    Fix the default login styling

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'text-center my-2 hover:scale-105 hover:opacity-55 transition-all rounded-lg py-2 px-4 bg-accent font-bold text-xl']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'text-center my-2 hover:scale-105 hover:opacity-55 transition-all rounded-lg py-2 px-4 bg-accent font-bold text-xl text-white']) }}>
     {{ $slot }}
 </button>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 bg-stockhive-grey rounded-lg hover:border-accent hover:shadow-accent hover:shadow-bxs focus:ring-accent transition-all']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 bg-stockhive-grey rounded-lg hover:border-accent hover:shadow-accent hover:shadow-bxs focus:ring-accent transition-all text-white']) }}>
