74bbee9edc9fc81543c30cace847baaf9a26af3e
ibzm <ibzm99@outlook.com>
2024-12-09 16:46:58 +0000

    Path fix
    moved over deliveries outside of table so that you can look at over deliveries

--- resources/views/Logistics/index.blade.php
+++ resources/views/Logistics/index.blade.php
@@ -11,17 +11,17 @@
                 class="border-separate border-2 m-auto my-4 lg:w-[90%] w-full text-center border-grey border-spacing-2 md:border-spacing-8 bg-stockhive-grey rounded-lg">
                 <thead>
                     <tr>
-                    <th class="py-2 px-4">Order ID</th>
-                    <th class="py-2 px-4">Name</th>
+                        <th class="py-2 px-4">Order ID</th>
+                        <th class="py-2 px-4">Name</th>
                         <th class="py-2 px-4">Date/Time</th>
                         <th class="py-2 px-4">Actions</th>
-                        
+
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($orders as $order)
                         <tr>
-                        <td class="py-2 px-4">{{ $order->id }}</td>
+                            <td class="py-2 px-4">{{ $order->id }}</td>
                             <td class="py-2 px-4">{{ $order->user->first_name }} {{ $order->user->last_name }}</td>
                             <td class="py-2 px-4">{{ $order->date_time }}</td>
                             <td class="py-2 px-4 flex justify-center items-center gap-4">
@@ -34,11 +34,11 @@
                                     @csrf
                                     <x-primary-button>Create delivery Note</x-primary-button>
                                 </form>
-                                <form action="{{ route('logistics.overdelivery') }}" method="GET" class="inline-block">
-                                    @csrf
-                                    <x-primary-button>Go to Overdelivery</x-primary-button>
-                                </form>
                             </td>
+                            <form action="{{ route('logistics.overdelivery') }}" method="GET" class="inline-block">
+                                @csrf
+                                <x-primary-button>Overdeliveries</x-primary-button>
+                            </form>
                         </tr>
                     @endforeach
                 </tbody>
