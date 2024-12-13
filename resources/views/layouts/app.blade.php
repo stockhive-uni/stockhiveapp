9cb691de7889425f0a1f83d9641174a073f003b7
Web Extension <robertjenner5@outlook.com>
2024-12-13 18:54:49 +0000

    Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ ucwords(str_replace('-', ' ', explode('.', Route::currentRouteName())[0])) }}</title>
+        <title>StockHive | {{ ucwords(str_replace('-', ' ', explode('.', Route::currentRouteName())[0])) }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
